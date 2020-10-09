e67c19c42b155590ddc8a97edbcf70e77abb0ae3
Pål Hamel <pal.hamel@gmail.com>
2020-10-09 10:03:44 +0200

    function to register a new user

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -13,3 +13,22 @@ export const loginRequired = (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized user!" });
   }
 };
+
+// function to register a new user:
+export const register = (req, res) => {
+  // pass info body
+  const newUser = new User(req.body);
+  // create user
+  newUser.hashPassword = bcrypt.hashSync(req.body.password, 10;
+  newUser.save((err, user) => {
+    if (err) {
+      return res.status(400).send({
+        message: err
+      });
+    } else {
+      user.hashPassword = undefined;
+      return res.json(user);
+    }
+  })
+  
+}

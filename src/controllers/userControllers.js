8ff62bf9ba254b3d3ec6db324679943255d460ec
Pål Hamel <pal.hamel@gmail.com>
2020-10-09 10:28:01 +0200

    function to login to api access

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -19,16 +19,47 @@ export const register = (req, res) => {
   // pass info body
   const newUser = new User(req.body);
   // create user
-  newUser.hashPassword = bcrypt.hashSync(req.body.password, 10;
+  newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
   newUser.save((err, user) => {
     if (err) {
       return res.status(400).send({
-        message: err
+        message: err,
       });
     } else {
       user.hashPassword = undefined;
       return res.json(user);
     }
-  })
-  
-}
+  });
+};
+
+// function to login to api access:
+export const login = (req, res) => {
+  // find a user in db
+  User.findOne(
+    {
+      email: req.body.email,
+    },
+    (err, user) => {
+      if (err) throw err;
+      if (!user) {
+        res
+          .status(401)
+          .json({ message: "Authentication failed. No user found." });
+      } else if (user) {
+        // if we find a user lets compare the passwords
+        if (!user.comparePassword(req.body.password, user.hashPassword)) {
+          res
+            .status(401)
+            .json({ message: "Authentication failed. Wrong password" });
+        } else {
+          return res.json({
+            token: jwt.sign(
+              { email: user.email, username: user.username, _id: user.id },
+              "RESTFULAPIs"
+            ),
+          });
+        }
+      }
+    }
+  );
+};

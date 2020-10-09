b253782040ba57f475bd88d2352e74daa5e11a88
Pål Hamel <pal.hamel@gmail.com>
2020-10-09 08:47:44 +0200

    function to check if user is logged in

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,9 +3,13 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { UserSchema } from "../models/userModel";
 
-
 const User = mongoose.model("User", UserSchema);
 
+// function to check if user is logged in:
 export const loginRequired = (req, res, next) => {
-
-}
+  if (req.user) {
+    next();
+  } else {
+    return res.status(401).json({ message: "Unauthorized user!" });
+  }
+};
